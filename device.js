b764d8e79c3afd210355a724433ceccb871c666f
Welleynise Dayenielle Vickers <w.d.vickers@se24.qmul.ac.uk>
2026-09-15 18:27:54 +0100

    Initial commit - Defined device script to continuously run without stopping every 5 seconds

---
+++ device.js
@@ -0,0 +1,15 @@
+// run device script to run continuously without stopping performing action every 5 seconds
+console.log(typeof functionCallRocky());
+setInterval(functionCallRocky, 5000);
+
+//define callback argument of type function to avoid undefined return type
+function functionCallRocky() {
+  console.log("Amaze Amaze Amaze");
+  const generateObject = {
+    device_id:  "",
+    status: "",
+    cpu_usage: ,
+  }
+}
+//test node js file works before entering logic
+ 
